6d5e38f17b5f51d436a1720c7b8b9bd64ce2f903
atatm <alelignayana@gmail.com>
2023-03-11 20:39:38 +0300

    make change rental.rb

--- rental.rb
+++ rental.rb
@@ -1,11 +1,13 @@
+require_relative 'book'
+
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, person, book)
+  def initialize(date, book, person)
     @date = date
     @book = book
+    book.rentals << self
     @person = person
     person.rentals << self
-    book.rentals << self
   end
 end
